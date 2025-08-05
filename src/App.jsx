2b5a77d1749c01e4cc07a98c130ced9cad21a6b1
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-05 19:42:28 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import StoreScreen from './screens/StoreScreen';
+
+function App() {
+  return (
+    <div>
+      <HomeScreen />
+      <StoreScreen />
+    </div>
+  );
+}
+
+export default App;
